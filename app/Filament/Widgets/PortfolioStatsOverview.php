e92a1c7b1f80054a82c849cae5ad1bdad5a93a1c
jalis <jalismahamud31@gmail.com>
2026-09-03 18:54:38 +0600

    changes made

--- app/Filament/Widgets/PortfolioStatsOverview.php
+++ app/Filament/Widgets/PortfolioStatsOverview.php
@@ -11,6 +11,15 @@ use App\Models\GalleryImage;
 use App\Models\Project;
 use App\Models\Skill;
 use App\Models\Technology;
+use App\Filament\Resources\BlogPosts\BlogPostResource;
+use App\Filament\Resources\Certifications\CertificationResource;
+use App\Filament\Resources\ContactSubmissions\ContactSubmissionResource;
+use App\Filament\Resources\Education\EducationResource;
+use App\Filament\Resources\Experiences\ExperienceResource;
+use App\Filament\Resources\GalleryImages\GalleryImageResource;
+use App\Filament\Resources\Projects\ProjectResource;
+use App\Filament\Resources\Skills\SkillResource;
+use App\Filament\Resources\Technologies\TechnologyResource;
 use Filament\Support\Icons\Heroicon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -24,23 +33,23 @@ class PortfolioStatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            $this->stat('Projects', Project::query()->count(), Heroicon::OutlinedRectangleStack, 'primary', 'projects.index'),
-            $this->stat('Blog posts', BlogPost::query()->count(), Heroicon::OutlinedNewspaper, 'info', 'blog-posts.index'),
-            $this->stat('Certifications', Certification::query()->count(), Heroicon::OutlinedAcademicCap, 'success', 'certifications.index'),
-            $this->stat('Education', Education::query()->count(), Heroicon::OutlinedBookOpen, 'warning', 'education.index'),
-            $this->stat('Experience', Experience::query()->count(), Heroicon::OutlinedBriefcase, 'gray', 'experiences.index'),
-            $this->stat('Skills', Skill::query()->count(), Heroicon::OutlinedSparkles, 'danger', 'skills.index'),
-            $this->stat('Technologies', Technology::query()->count(), Heroicon::OutlinedCodeBracket, 'primary', 'technologies.index'),
-            $this->stat('Gallery images', GalleryImage::query()->count(), Heroicon::OutlinedPhoto, 'info', 'gallery-images.index'),
-            $this->stat('Messages', ContactSubmission::query()->count(), Heroicon::OutlinedEnvelope, 'success', 'contact-submissions.index'),
+            $this->stat('Projects', Project::query()->count(), Heroicon::OutlinedRectangleStack, 'primary', ProjectResource::class),
+            $this->stat('Blog posts', BlogPost::query()->count(), Heroicon::OutlinedNewspaper, 'info', BlogPostResource::class),
+            $this->stat('Certifications', Certification::query()->count(), Heroicon::OutlinedAcademicCap, 'success', CertificationResource::class),
+            $this->stat('Education', Education::query()->count(), Heroicon::OutlinedBookOpen, 'warning', EducationResource::class),
+            $this->stat('Experience', Experience::query()->count(), Heroicon::OutlinedBriefcase, 'gray', ExperienceResource::class),
+            $this->stat('Skills', Skill::query()->count(), Heroicon::OutlinedSparkles, 'danger', SkillResource::class),
+            $this->stat('Technologies', Technology::query()->count(), Heroicon::OutlinedCodeBracket, 'primary', TechnologyResource::class),
+            $this->stat('Gallery images', GalleryImage::query()->count(), Heroicon::OutlinedPhoto, 'info', GalleryImageResource::class),
+            $this->stat('Messages', ContactSubmission::query()->count(), Heroicon::OutlinedEnvelope, 'success', ContactSubmissionResource::class),
         ];
     }
 
-    private function stat(string $label, int $value, Heroicon $icon, string $color, string $route): Stat
+    private function stat(string $label, int $value, Heroicon $icon, string $color, string $resource): Stat
     {
         return Stat::make($label, $value)
             ->icon($icon)
             ->color($color)
-            ->url(route($route));
+            ->url($resource::getUrl('index'));
     }
 }
